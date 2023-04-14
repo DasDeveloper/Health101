bd2eadf55152501044fa307d33777f56a249b76c
DasDeveloper <rohandas@live.ca>
2023-04-13 20:22:15 -0400

    Added frontend navbar

---
+++ client/src/components/navbar.js
@@ -0,0 +1,14 @@
+import "../css/navbar.css"
+
+
+
+const Navbar = () =>{
+    return(
+        <div className="navbar">
+           <h1>Health101</h1>
+        </div>
+    )
+}
+
+
+export default Navbar;
