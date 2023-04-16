e15f3c7b5912a93b7493355b919022c53cd6ef4e
DasDeveloper <rohandas@live.ca>
2023-04-16 17:27:32 -0400

    changes to changeUsername page

--- client/src/components/changeUsername.jsx
+++ client/src/components/changeUsername.jsx
@@ -6,12 +6,12 @@ const ChangeUsername = () =>{
     return(
         <div className="username">
             
-            <p className="title">Change your username</p>
+            <p className="title"><h3>Change your username</h3></p>
             <br/>
 
             <p><b>New username:</b></p>
             <br/>
-            <input></input>
+            <input placeholder="New username"></input>
             <br/>
             <button>Confirm</button>
             
