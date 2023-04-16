6addd03ff3c1a460ecd9e042db2b740e113e76b8
DasDeveloper <rohandas@live.ca>
2023-04-16 18:22:00 -0400

    changes to settings page

--- client/src/components/settings.jsx
+++ client/src/components/settings.jsx
@@ -1,14 +1,36 @@
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import '../css/settings.css'
+import { useNavigate } from "react-router-dom";
+
+
+
+
 
 const Settings = () =>{
+
+    const navigate = useNavigate();
+
+    const navigateToChangeUsername = () =>{
+
+        navigate("/changeusername")
+    }
+
+    const navigateToChangePassword = () =>{
+
+        navigate("/changepassword")
+    }
+    const navigateToChangeCalories = () =>{
+
+        navigate("/changeminimumcalories")
+    }
+
     return (
         <div>
 
             <div className="iconContainer"><AccountCircleIcon className="accountIcon" fontSize="large"></AccountCircleIcon></div>
-            <button className="change">Change your username</button>
-            <button className="change">Change your password</button>
-            <button className="change">Change your minimum calories intake</button>
+            <button className="change" onClick={navigateToChangeUsername}>Change your username</button>
+            <button className="change" onClick={navigateToChangePassword}>Change your password</button>
+            <button className="change" onClick={navigateToChangeCalories}>Change your minimum calories intake</button>
         </div>
     );
 }
