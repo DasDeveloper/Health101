7533944d3c6c3ec38bac49e15e5cb98c538b6f7a
AnnaHsu1 <annahsu2000@hotmail.com>
2023-04-16 22:42:12 -0400

    CSS changes

--- client/src/components/homePage.js
+++ client/src/components/homePage.js
@@ -13,12 +13,12 @@ const HomePage = () => {
       <Navbar /> 
         <div className={HomePageCSS.container}>
           <div className={HomePageCSS.imageContainer}>
-            <img className={HomePageCSS.image} src="https://www.shutterstock.com/shutterstock/videos/1039301405/thumb/12.jpg?ip=x480" 
+            <img className={HomePageCSS.image} src="https://s3.amazonaws.com/cdn.freshdesk.com/data/helpdesk/attachments/production/13116362801/original/adOkBGpSzksF1vBt2RnHyauy4YM6o87_fQ.jpeg?1636844459" 
             alt="This is an image" />
-            <div className={HomePageCSS.textContainer}>
+            {/* <div className={HomePageCSS.textContainer}>
               <p style={{display:"block", textAlign:"center"}}><b>Calories</b></p>
               <p style={{display:"block"}}><b>Consumed:</b> 1002 <b>Burnt:</b> 500  <b>Net:</b> 502</p>
-            </div>
+            </div> */}
             
           </div>
           <div className={HomePageCSS.menu}>
