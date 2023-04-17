4619cb897830cd09e7987f1c57862a23f9b5b485
Niniishere <annieliu190@gmail.com>
2023-04-16 20:53:16 -0400

    fixed content alignment

--- client/src/components/notification.js
+++ client/src/components/notification.js
@@ -3,6 +3,9 @@ import Footer from "./footer";
 import NotificationCSS from "../css/notification.module.css";
 import { TextField } from "@mui/material";
 import { grey } from "@mui/material/colors";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { Link } from "react-router-dom";
+import AddIcon from "@mui/icons-material/Add";
 
 const NotificationPage = () => {
   return (
@@ -18,6 +21,26 @@ const NotificationPage = () => {
             <b>Date and time:</b>
             <TextField id="texfield" variant="outlined" />
           </p>
+        </div>
+      </div>
+      <div className={NotificationCSS.bottom}>
+        <Link to="/activities">
+          <div className={NotificationCSS.left}>
+            <button className={NotificationCSS["button"]} id={NotificationCSS["back"]}>
+              <ArrowBackIcon fontSize="large" />
+            </button>
+            <label>
+              <b>Go Back</b>
+            </label>
+          </div>
+        </Link>
+        <div className={NotificationCSS.right}>
+          <button className={NotificationCSS["button"]}>
+            <AddIcon fontSize="large" />
+          </button>
+          <label>
+            <b>Add Activity</b>
+          </label>
         </div>
       </div>
       <Footer />
