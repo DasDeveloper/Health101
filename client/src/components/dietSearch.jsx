1a338df294804658ca80d858c73b9c28dc8dd052
Niniishere <annieliu190@gmail.com>
2023-04-17 01:23:26 -0400

    fixed slideshow

--- client/src/components/dietSearch.jsx
+++ client/src/components/dietSearch.jsx
@@ -3,10 +3,15 @@ import Footer from "./footer";
 import DietSearchCSS from "../css/dietSearch.module.css";
 import { TextField, IconButton, InputAdornment } from "@mui/material";
 import { FaSearch } from "react-icons/fa";
-import { Carousel } from "react-responsive-carousel";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
 
 const DietSearchPage = () => {
-  const images = ["https://images.immediate.co.uk/production/volatile/sites/30/2020/08/paneer-korma-07a67d4.jpg?quality=90&resize=768,574", "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/paneer-korma-07a67d4.jpg?quality=90&resize=768,574", "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/paneer-korma-07a67d4.jpg?quality=90&resize=768,574"];
+  const glutenfree = [<img src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/paneer-korma-07a67d4.jpg?quality=90&resize=768,574" width={120} height={120} onDragStart={e => e.preventDefault()} role="presentation" />, <img src="https://sweetpotatosoul.com/wp-content/uploads/2021/01/Massaged-Kale-Salad-2-Fast-Vegan-Meal-Prep.jpeg" width={120} height={120} onDragStart={e => e.preventDefault()} role="presentation" />, <img src="https://images.immediate.co.uk/production/volatile/sites/2/2019/12/Vegan-Thai-red-curry-a716ab7-scaled.jpg?quality=90&crop=17px,45px,2525px,1086px&resize=556,505" width={120} height={120} onDragStart={e => e.preventDefault()} role="presentation" />];
+
+  const lowcarb = [<img src="https://assets.rebelmouse.io/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHBpcmVzX2F0IjoxNjgyMzIzNTY5LCJpbWFnZSI6Imh0dHBzOi8vYXNzZXRzLnJibC5tcy8yMjg1MDc4My9vcmlnaW4uanBnIn0.TBisjNoPThfIv9S5EOo7rFF1Xa4v7__fnPjluWD_xlQ/img.jpg" width={120} height={120} onDragStart={e => e.preventDefault()} role="presentation" />, <img src="https://www.maebells.com/wp-content/uploads/2020/02/skewers-4.jpg" width={120} height={120} onDragStart={e => e.preventDefault()} role="presentation" />, <img src="https://i0.wp.com/post.healthline.com/wp-content/uploads/2020/04/low-carb-diet-meal-plan-and-menu-1296x728-feature.jpg?h=1528" width={120} height={120} onDragStart={e => e.preventDefault()} role="presentation" />];
+
+  const buildmuscle = [<img src="https://images.immediate.co.uk/production/volatile/sites/2/2022/08/Healthy-thai-Salmon-TrayBake-deb98b5.jpg?quality=90&resize=556,505" width={120} height={120} onDragStart={e => e.preventDefault()} role="presentation" />, <img src="https://i0.wp.com/emyogifit.com/wp-content/uploads/2022/05/IMG_1739.jpg?fit=750%2C810&ssl=1" width={120} height={120} onDragStart={e => e.preventDefault()} role="presentation" />, <img src="https://hips.hearstapps.com/hmg-prod/images/thai-tempeh-buddha-bowl-6-1-683x1024-1594747155.jpg?resize=480:*" width={120} height={120} onDragStart={e => e.preventDefault()} role="presentation" />];
 
   return (
     <div className={DietSearchCSS.body}>
@@ -30,23 +35,17 @@ const DietSearchPage = () => {
             />
           </p>
         </div>
-        <div className="slideshow-section1">
-          <p>
-            <b> Gluten free: </b>
-          </p>
-          <br />
-          <Carousel>
-            <div>
-              <img src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/paneer-korma-07a67d4.jpg?quality=90&resize=768,574" width={150} height={150} />
-            </div>
-            <div>
-              <img src="https://sweetpotatosoul.com/wp-content/uploads/2021/01/Massaged-Kale-Salad-2-Fast-Vegan-Meal-Prep.jpeg" width={150} height={150} />
-            </div>
-            <div>
-              <img src="https://images.immediate.co.uk/production/volatile/sites/2/2019/12/Vegan-Thai-red-curry-a716ab7-scaled.jpg?quality=90&crop=17px,45px,2525px,1086px&resize=556,505" width={150} height={150} />
-            </div>
-          </Carousel>
+        <div className={DietSearchCSS.slideshowSection}>
+          <b> Gluten free: </b>
+          <AliceCarousel mouseTracking items={glutenfree} />
+          <b> Low carbs: </b>
+          <AliceCarousel mouseTracking items={lowcarb} />
+          <b> Build muscles: </b>
+          <AliceCarousel mouseTracking items={buildmuscle} />
         </div>
+        <br></br>
+        <br></br>
+        <br></br>
       </div>
       <Footer />
     </div>
