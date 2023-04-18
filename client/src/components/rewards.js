4fa6b13dae1e72738a207e9436fe8476918fc24a
Niniishere <annieliu190@gmail.com>
2023-04-18 15:53:51 -0400

    fixed the rewards fields

--- client/src/components/rewards.js
+++ client/src/components/rewards.js
@@ -15,19 +15,19 @@ const RewardsPage = () => {
         </div>
         <div className={RewardsCSS.dropdown}>
           <FormControl className={RewardsCSS.choice}>
-            <InputLabel>Amazon</InputLabel>
-            <Select sx={{ fontWeight: "bold", height: "75px" }} label="Amazon">
-              <MenuItem value={20}>20$ (5000 points)</MenuItem>
-              <MenuItem value={50}>50$ (15 000 points)</MenuItem>
-              <MenuItem value={100}>100$ (25 000 points)</MenuItem>
+            <InputLabel>Cards</InputLabel>
+            <Select sx={{ fontWeight: "bold", height: "75px" }} label="Cards">
+              <MenuItem value="Mastercard">Amazon</MenuItem>
+              <MenuItem value="Visa">Visa</MenuItem>
+              <MenuItem value="Interact">Mastercard</MenuItem>
             </Select>
           </FormControl>
           <FormControl className={RewardsCSS.choice}>
-            <InputLabel>Cards</InputLabel>
-            <Select sx={{ fontWeight: "bold", height: "75px" }} label="Cards">
-              <MenuItem value="Mastercard">Mastercard</MenuItem>
-              <MenuItem value="Visa">Visa</MenuItem>
-              <MenuItem value="Interact">Interact</MenuItem>
+            <InputLabel>Amount</InputLabel>
+            <Select sx={{ fontWeight: "bold", height: "75px" }} label="Amount">
+              <MenuItem value={20}>20$ (5000 points)</MenuItem>
+              <MenuItem value={50}>50$ (15 000 points)</MenuItem>
+              <MenuItem value={100}>100$ (25 000 points)</MenuItem>
             </Select>
           </FormControl>
         </div>
