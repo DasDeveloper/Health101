3e38ea8d3a7f738a3874fcfb5a240bee72c67880
AnnaHsu1 <annahsu2000@hotmail.com>
2023-04-19 18:22:46 -0400

    Added navigation to home page and footer

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -2,13 +2,33 @@ import FooterCSS from "../css/footer.module.css";
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import HomeIcon from '@mui/icons-material/Home';
 import SettingsIcon from '@mui/icons-material/Settings';
+import { useNavigate } from "react-router-dom";
+
 
 const Footer = () =>{
+
+  const navigate = useNavigate();
+  
+  const redirectHomePage = () => {
+    navigate("/");
+  };
+
+  const redirectSettings = () => {
+    navigate("/settings");
+  };
+
+  const redirectAccount = () => {
+    navigate("/changeminimumcalories");
+  };
+
+
+
+
   return (
     <div className={FooterCSS.footer}>
-      <AccountCircleIcon className={FooterCSS.icon} fontSize="large"></AccountCircleIcon>
-      <HomeIcon className={FooterCSS.icon} fontSize="large"></HomeIcon>
-      <SettingsIcon className={FooterCSS.icon} fontSize="large"></SettingsIcon>
+      <AccountCircleIcon className={FooterCSS.icon} fontSize="large" onClick={redirectAccount}></AccountCircleIcon>
+      <HomeIcon className={FooterCSS.icon} fontSize="large" onClick={redirectHomePage}></HomeIcon>
+      <SettingsIcon className={FooterCSS.icon} fontSize="large" onClick={redirectSettings}></SettingsIcon>
     </div>
   );
 }
