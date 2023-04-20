a827981c2fe1a99033aaf0047c114743c56125b6
DasDeveloper <rohandas@live.ca>
2023-04-19 22:50:24 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ app.set("trust proxy", 1);
 // );
 
 const allowCrossDomain = (req, res, next) => {
-  res.header(`Access-Control-Allow-Origin`, `example.com`);
+  res.header(`Access-Control-Allow-Origin`, `https://health101-frontend.vercel.app`);
   res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
   res.header(`Access-Control-Allow-Headers`, `Content-Type`);
   next();
