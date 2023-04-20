ed2532c76c4253441afb7e4cf114ec06f26c881f
DasDeveloper <rohandas@live.ca>
2023-04-19 20:58:18 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const session = require("express-session");
 const cors = require("cors");
-const User = require("./models/user");
-const MongoDBStore = require("connect-mongodb-session")(session);
+// const User = require("./models/user");
+// const MongoDBStore = require("connect-mongodb-session")(session);
 
 const app = express();
 app.use(express.json());
@@ -12,31 +12,31 @@ const port = 9000;
 dotenv.config();
 
 //Session length
-const session_length = 1000 * 60 * 60;
+// const session_length = 1000 * 60 * 60;
 
 // //Set proxy
 // app.set("trust proxy", 1);
 
 //MongoDB seission store
-const mongoDBstore = new MongoDBStore({
-    uri: process.env.DATABASE,
-    collection: "userSessions",
-});
+// const mongoDBstore = new MongoDBStore({
+//     uri: process.env.DATABASE,
+//     collection: "userSessions",
+// });
 
-app.use(
-    session({
-        secret: "secret123",
-        name: "user_session_id",
-        store: mongoDBstore,
-        cookie: {
-            maxAge: session_length,
-            sameSite: false,
-            secure: true,
-        },
-        resave: true,
-        saveUninitialized: false,
-    })
-);
+// app.use(
+//     session({
+//         secret: "secret123",
+//         name: "user_session_id",
+//         store: mongoDBstore,
+//         cookie: {
+//             maxAge: session_length,
+//             sameSite: false,
+//             secure: true,
+//         },
+//         resave: true,
+//         saveUninitialized: false,
+//     })
+// );
 
 app.use(
     cors({
@@ -66,8 +66,8 @@ res.send("Hello World!");
 });
 
 //Routes for our API endpoints
-app.use("/users", require("./routes/userRoutes.js"));
-app.use("/session", require("./routes/sessionRoutes.js"));
+// app.use("/users", require("./routes/userRoutes.js"));
+// app.use("/session", require("./routes/sessionRoutes.js"));
 app.use("/food", require("./routes/caloriesRoutes.js"))
 //Running the server
 const server = app.listen(port, () => {
